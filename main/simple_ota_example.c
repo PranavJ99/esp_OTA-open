0d72cdf5aa6d5a7ebe187dfe691bf3f15b7aefcc
Your Name <you@example.com>
2021-08-12 12:19:05 +0530

    directories

---
+++ main/simple_ota_example.c
@@ -0,0 +1,248 @@
+/* OTA example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_legacy.h"
+#include "esp_log.h"
+#include "esp_ota_ops.h"
+#include "esp_http_client.h"
+#include "esp_https_ota.h"
+#include "protocol_examples_common.h"
+#include "string.h"
+#include "cJSON.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "wifi_functions.h"
+#include "protocol_examples_common.h"
+#include <sys/socket.h>
+//#if CONFIG_EXAMPLE_CONNECT_WIFI
+//#include "esp_wifi.h"
+//#endif
+
+#define HASH_LEN 32
+
+
+
+
+#define FIRMWARE_VERSION	0.1
+#define UPDATE_JSON_URL	"https://drive.google.com/file/d/1GyG3ij7R3-okmm-gF96ioahyM_IJ4tr2/view?usp=sharing"
+
+
+
+
+#ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF
+/* The interface name value can refer to if_desc in esp_netif_defaults.h */
+#if CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF_ETH
+static const char *bind_interface_name = "eth";
+#elif CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF_STA
+static const char *bind_interface_name = "sta";
+#endif
+#endif
+
+
+
+// server certificates
+//extern const char server_cert_pem_start[] asm("_binary_certs_pem_start");
+//extern const char server_cert_pem_end[] asm("_binary_certs_pem_end");
+// receive buffer
+char rcv_buffer[200];
+
+
+
+
+static const char *TAG = "simple_ota_example";
+
+#define OTA_URL_SIZE 256
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    switch (evt->event_id) {
+    case HTTP_EVENT_ERROR:
+        ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+        break;
+    case HTTP_EVENT_ON_CONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+        break;
+    case HTTP_EVENT_HEADER_SENT:
+        ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+        break;
+    case HTTP_EVENT_ON_HEADER:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+        break;
+    case HTTP_EVENT_ON_DATA:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+        break;
+    case HTTP_EVENT_ON_FINISH:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+        break;
+    case HTTP_EVENT_DISCONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+        break;
+    }
+    return ESP_OK;
+}
+
+void check_update_task(void *pvParameter) {
+	
+	while(1) {
+        
+		printf("Looking for a new firmware...\n");
+	
+		// configure the esp_http_client
+		esp_http_client_config_t config = {
+        .url = UPDATE_JSON_URL,
+        .event_handler = _http_event_handler,
+		};
+		esp_http_client_handle_t client = esp_http_client_init(&config);
+	
+		// downloading the json file
+		esp_err_t err = esp_http_client_perform(client);
+		if(err == ESP_OK) {
+			
+			// parse the json file	
+			cJSON *json = cJSON_Parse(rcv_buffer);
+			if(json == NULL) printf("downloaded file is not a valid json, aborting...\n");
+			else {	
+				cJSON *version = cJSON_GetObjectItemCaseSensitive(json, "version");
+				cJSON *file = cJSON_GetObjectItemCaseSensitive(json, "file");
+				
+				// check the version
+				if(!cJSON_IsNumber(version)) printf("unable to read new version, aborting...\n");
+				else {
+					
+					double new_version = version->valuedouble;
+					if(new_version > FIRMWARE_VERSION) {
+						
+						printf("current firmware version (%.1f) is lower than the available one (%.1f), upgrading...\n", FIRMWARE_VERSION, new_version);
+						if(cJSON_IsString(file) && (file->valuestring != NULL)) {
+							printf("downloading and installing new firmware (%s)...\n", file->valuestring);
+							
+							esp_http_client_config_t ota_client_config = {
+								.url = file->valuestring,
+								//.cert_pem = server_cert_pem_start,
+							};
+							esp_err_t ret = esp_https_ota(&ota_client_config);
+							if (ret == ESP_OK) {
+								printf("OTA OK, restarting...\n");
+								esp_restart();
+							} else {
+								printf("OTA failed...\n");
+							}
+						}
+						else printf("unable to read the new file name, aborting...\n");
+					}
+					else printf("current firmware version (%.1f) is greater or equal to the available one (%.1f), nothing to do...\n", FIRMWARE_VERSION, new_version);
+				}
+			}
+		}
+		else printf("unable to download the json file, aborting...\n");
+		
+		// cleanup
+		esp_http_client_cleanup(client);
+		
+		printf("\n");
+        vTaskDelay(30000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void print_sha256(const uint8_t *image_hash, const char *label)
+{
+    char hash_print[HASH_LEN * 2 + 1];
+    hash_print[HASH_LEN * 2] = 0;
+    for (int i = 0; i < HASH_LEN; ++i) {
+        sprintf(&hash_print[i * 2], "%02x", image_hash[i]);
+    }
+    ESP_LOGI(TAG, "%s %s", label, hash_print);
+}
+
+static void get_sha256_of_partitions(void)
+{
+    uint8_t sha_256[HASH_LEN] = { 0 };
+    esp_partition_t partition;
+
+    // get sha256 digest for bootloader
+    partition.address   = ESP_BOOTLOADER_OFFSET;
+    partition.size      = ESP_PARTITION_TABLE_OFFSET;
+    partition.type      = ESP_PARTITION_TYPE_APP;
+    esp_partition_get_sha256(&partition, sha_256);
+    print_sha256(sha_256, "SHA-256 for bootloader: ");
+
+    // get sha256 digest for running partition
+    esp_partition_get_sha256(esp_ota_get_running_partition(), sha_256);
+    print_sha256(sha_256, "SHA-256 for current firmware: ");
+}
+
+void app_main(void)
+{
+
+
+// connect to the wifi network
+	wifi_initialise();
+	wifi_wait_connected();
+	printf("Connected to wifi network.............................................................\n");
+	
+
+
+
+
+
+    // Initialize NVS.
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        // 1.OTA app partition table has a smaller NVS partition size than the non-OTA
+        // partition table. This size mismatch may cause NVS initialization to fail.
+        // 2.NVS partition contains data in new format and cannot be recognized by this version of code.
+        // If this happens, we erase NVS partition and initialize NVS again.
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+   
+   
+    ESP_ERROR_CHECK(err);
+
+    get_sha256_of_partitions();
+
+/*    ESP_ERROR_CHECK(esp_netif_init());		*/
+  //  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+  /*  ESP_ERROR_CHECK(example_connect());	*/
+
+
+
+
+//calling for update
+ xTaskCreate(&check_update_task, "check_update_task", 8192, NULL, 5, NULL);
+
+
+
+
+
+
+//#if CONFIG_EXAMPLE_CONNECT_WIFI
+    /* Ensure to disable any WiFi power save mode, this allows best throughput
+     * and hence timings for overall OTA operation.
+     */
+//    esp_wifi_set_ps(WIFI_PS_NONE);
+//#endif // CONFIG_EXAMPLE_CONNECT_WIFI 	
+
+
+
+
+
+}
+
+
+
+
